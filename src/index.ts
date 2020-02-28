a8ba9c2c035485a81f6ce06d8d63e095939ed66f
Hirofumi Shibata <33661448+shiba-hiro@users.noreply.github.com>
2020-02-28 14:48:33 +0900

    Add error handlers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 
 import Logger from './Logger';
 
@@ -6,8 +6,8 @@ const app: express.Application = express();
 const port: string = process.env.PORT || '3000';
 
 app.use(express.json());
-app.get('/health-check', (_, res: Response) => {
-  Logger.info('health-check endpoint is called');
+app.get('/health', (_, res: Response) => {
+  Logger.info('health endpoint is called');
 
   const appState = {
     message: 'Application is running.',
@@ -18,6 +18,28 @@ app.get('/health-check', (_, res: Response) => {
     .status(200)
     .send({
       app: appState,
+    });
+});
+
+app.use((req: Request, res: Response) => {
+  const message = `No route fond: ${req.path}`;
+  Logger.info(message);
+  res
+    .status(404)
+    .send({
+      message,
+    });
+});
+
+// To use as an error request handler, the function needs to have 4 arguments.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _: express.NextFunction) => {
+  Logger.error(`Unexpected error! ${req.path}`);
+  Logger.error(err);
+  res
+    .status(500)
+    .send({
+      message: 'Unexpected error occurred. Please retry after a while.',
     });
 });
 
