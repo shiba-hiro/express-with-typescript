c33e60350b10ced47fd81c2def2aba0ebf389938
Hirofumi Shibata <33661448+shiba-hiro@users.noreply.github.com>
2020-06-19 17:02:57 +0900

    Add pm2

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,6 +1,16 @@
 import log4js from 'log4js';
 
-const logger = log4js.getLogger();
-logger.level = process.env.LOG_LEVEL || 'debug';
+log4js.addLayout('json', () => (logEvent) => JSON.stringify(logEvent));
 
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout', layout: { type: 'json' } },
+  },
+  categories: {
+    default: { appenders: ['out'], level: process.env.LOG_LEVEL || 'debug' },
+  },
+  pm2: process.env.NODE_ENV === 'production',
+});
+
+const logger = log4js.getLogger();
 export default logger;
